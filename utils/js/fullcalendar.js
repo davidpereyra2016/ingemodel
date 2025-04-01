b0416339bad71a0d0465ccdc375b84873eb4599f
Ronaldo Almirón <rony.almiron.015@gmail.com>
2025-04-01 17:28:57 -0300

    Actualizar el calendario para mostrar múltiples meses y mejorar la configuración de eventos

--- utils/js/fullcalendar.js
+++ utils/js/fullcalendar.js
@@ -1,12 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth',
+    const calendarEl = document.getElementById('calendarInicio')
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'multiMonthYear',
         headerToolbar: {
             left: 'prev,next today',
             center: 'title',
-            right: 'dayGridMonth,dayGridWeek,dayGridDay'
-        }
-    });
-    calendar.render();
-});
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        },
+        views: {
+            multiMonthYear: {
+                type: 'multiMonthYear',
+                duration: {
+                    months: 6
+                },
+                duration: {
+                    months: 6
+                },
+                buttonText: '6 meses'
+            }
+        },
+        locale: 'es',
+        initialDate: new Date(),
+        editable: true,
+        selectable: true,
+        selectMirror: true,
+        dayMaxEvents: true, // allow "more" link when too many events
+
+    })
+    calendar.render()
+})
