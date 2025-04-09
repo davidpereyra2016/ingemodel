e863b821c9c50230b6a42febaa5bdd8fbf481057
Ronaldo Almirón <rony.almiron.015@gmail.com>
2025-04-09 08:21:42 -0300

    se actualizó el carrusel en la página de inicio con nuevas imágenes y estilos, se eliminaron imágenes obsoletas

--- vistas/paginas/inicio.php
+++ vistas/paginas/inicio.php
@@ -1,15 +1,15 @@
 <div id="carouselEvento" class="carousel slide hero-carousel" data-bs-ride="carousel" style="height: 400px; overflow: hidden;">
-    <div class="carousel-indicators">
+    <div class="carousel-indicators bg-success-theme">
         <button type="button" data-bs-target="#carouselEvento" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#carouselEvento" data-bs-slide-to="1" aria-label="Slide 2"></button>
         <button type="button" data-bs-target="#carouselEvento" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="./assets/img/lugar-evento-2.jpg" class="d-block w-100" alt="Imagen 1">
-            <div class="card-img-overlay color-white w-100 h-100 d-flex flex-column align-items-center text-center mt-30" style="top: 0; background-color: rgba(0, 0, 0, 0.5); padding-top: 8rem;">
-                <h2 class="mt-10" style="color: #fff; font-size: 3rem;">Reserva de Salón de Eventos</h2>
-                <p class=" color-white" style="color: #fff; font-size: 1.5rem;">Selecciona una fecha disponible para tu evento</p>
+            <img src="./assets/img/slider-1.jpg" class="d-block w-100 hero-img-overlay object-fit-fill" alt="Imagen 1">
+            <div class="card-img-overlay card-img-overlay-theme color-white d-flex flex-column align-items-center text-center mt-30">
+                <h2 class="card-title color-white b5 mt-10 mb-3">Reserva de Salón de Eventos</h2>
+                <p class="card-subtitle color-white mb-3">Selecciona una fecha disponible para tu evento</p>
                 <!-- <a href="index.php?controlador=reservas&accion=crear" class="btn btn-success btn-lg">Reservar</a> -->
                 <button class="btn btn-lg btn-success-theme" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
                     <i class="bi bi-calendar2-plus"></i>
@@ -18,19 +18,25 @@
             </div>
         </div>
         <div class="carousel-item">
-            <img src="./assets/img/lugar-evento.jpg" class="d-block w-100 hero-img-overlay" alt="Imagen 2">
-            <div class="card-img-overlay color-white w-100 h-100 d-flex flex-column align-items-center text-center mt-30" style="top: 0; background-color: rgba(0, 0, 0, 0.5); padding-top: 8rem;">
-                <h2 class="mt-10" style="color: #fff; font-size: 3rem;">Reserva de Salón de Eventos</h2>
-                <p class=" color-white" style="color: #fff; font-size: 1.5rem;">Selecciona una fecha disponible para tu evento</p>
-                <a href="index.php?controlador=reservas&accion=crear" class="btn btn-success btn-lg">Reservar</a>
+            <img src="./assets/img/slider-3.jpg" class="d-block w-100 hero-img-overlay object-fit-cover" alt="Imagen 2">
+            <div class="card-img-overlay card-img-overlay-theme color-white d-flex flex-column align-items-center text-center mt-30">
+                <h2 class="card-title color-white mt-10 mb-3">Reserva Cancha de Futbol</h2>
+                <p class="card-subtitle color-white mb-3">Selecciona una fecha y hora disponible para tu partido</p>
+                <button class="btn btn-lg btn-success-theme" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+                    <i class="bi bi-calendar2-plus"></i>
+                    <span class="ms-2">Reservar</span>
+                </button>
             </div>
         </div>
         <div class="carousel-item">
-            <img src="./assets/img/lugar-evento-2.jpg" class="d-block w-100 hero-img-overlay" alt="Imagen 3">
-            <div class="card-img-overlay color-white w-100 h-100 d-flex flex-column align-items-center text-center mt-30" style="top: 0; background-color: rgba(0, 0, 0, 0.5); padding-top: 8rem;">
-                <h2 class="mt-10" style="color: #fff; font-size: 3rem;">Reserva de Salón de Eventos</h2>
-                <p class=" color-white" style="color: #fff; font-size: 1.5rem;">Selecciona una fecha disponible para tu evento</p>
-                <a href="index.php?controlador=reservas&accion=crear" class="btn btn-success btn-lg">Reservar</a>
+            <img src="./assets/img/slider-2.jpg" class="d-block w-100 hero-img-overlay object-fit-cover" alt="Imagen 3">
+            <div class="card-img-overlay card-img-overlay-theme color-white d-flex flex-column align-items-center text-center mt-30">
+                <h2 class="card-title color-white mt-10 mb-3">Reserva de Salón de Eventos</h2>
+                <p class="card-subtitle color-white mb-3">Selecciona una fecha disponible para tu evento</p>
+                <button class="btn btn-lg btn-success-theme" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+                    <i class="bi bi-calendar2-plus"></i>
+                    <span class="ms-2">Reservar</span>
+                </button>
             </div>
         </div>
     </div>
