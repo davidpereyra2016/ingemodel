c70a9218b519373e06cfcf000efe31c2e9f9469d
Ronaldo Almirón <rony.almiron.015@gmail.com>
2025-04-22 10:55:17 -0300

    se mejoró el menú de reservas y se añadieron íconos a los elementos del dropdown, se ajustó la lógica de visualización para ingenieros y se mantuvo la funcionalidad del dropdown de notificaciones

--- vistas/partials/header.php
+++ vistas/partials/header.php
@@ -22,32 +22,39 @@
             Reservas
           </a>
           <ul class="dropdown-menu text-uppercase" data-bs-popper="static">
-            <li><a class="dropdown-item" href="?controlador=reservas&accion=calendario">Salón</a></li>
-            <li><a class="dropdown-item" href="?controlador=reservas&accion=listar">Cancha</a></li>
+            <li><a class="dropdown-item" href="?controlador=reservas&accion=calendario"><i class="bi bi-calendar-event me-2"></i> Salón</a></li>
+            <li><a class="dropdown-item" href="?controlador=reservas&accion=listar" disabled> <i class="bi bi-calendar-event-fill me-2"></i>Cancha</a></li>
           </ul>
         </li>
-
-        <li class="nav-item dropdown">
+        
+        <?php if (!$esIngeniero): ?>
+        <li class="nav-item dropdown text-uppercase">
           <a class="nav-link dropdown-toggle text-uppercase <?php echo $controlador == 'reservas' && ($accion == 'listar' || $accion == 'listar&tipo=2') ? 'active' : ''; ?>" role="button" data-bs-toggle="dropdown" aria-expanded="true">
             Gestión
           </a>
           <ul class="dropdown-menu" data-bs-popper="static">
-            <li><a class="dropdown-item" href="?controlador=reservas&accion=listar&tipo=1">Mis Reservas</a></li>
-            <li><a class="dropdown-item" href="?controlador=reservas&accion=listar&tipo=2">Gestionar Reservas</a></li>
+            <li><a class="dropdown-item" href="?controlador=reservas&accion=listar&tipo=1"><i class="bi bi-calendar2-event me-2"></i> Mis Reservas</a></li>
+            <li><a class="dropdown-item" href="?controlador=reservas&accion=listar&tipo=2"> <i class="bi bi-calendar-range-fill me-2"></i> Gestionar Reservas</a></li>
           </ul>
         </li>
+        <?php else: ?>
+          <li class="nav-item">
+            <a class="nav-link text-uppercase <?php echo $controlador == 'reservas' && $accion == 'listar' ? 'active' : ''; ?>"
+              aria-current="page" href="?controlador=reservas&accion=listar"><i class="bi bi-calendar2-event me-2"></i>Mis Reservas</a>
+          </li>         
+        <?php endif; ?>
         <!-- <li class="nav-item py-2 py-lg-1 col-12 col-lg-auto">
           <div class="vr d-none d-lg-flex h-100 mx-lg-2 text-white"></div>
           <hr class="d-lg-none my-2 text-white-50">
         </li> -->
 
-        <li class="nav-item dropdown text-uppercase">
-          <a class="nav-link dropdown-toggle <?php echo $controlador == 'configuracion' ? 'active' : ''; ?>" role="button" data-bs-toggle="dropdown" aria-expanded="true">
-            <i class="bi bi-gear-fill"></i>
-            Configuración
-          </a>
-          <ul class="dropdown-menu" data-bs-popper="static">
-            <?php if (!$esIngeniero): ?>
+        <?php if (!$esIngeniero): ?>
+          <li class="nav-item dropdown text-uppercase">
+            <a class="nav-link dropdown-toggle <?php echo $controlador == 'configuracion' ? 'active' : ''; ?>" role="button" data-bs-toggle="dropdown" aria-expanded="true">
+              <i class="bi bi-gear-fill"></i>
+              Configuración
+            </a>
+            <ul class="dropdown-menu" data-bs-popper="static">
               <li>
                 <a class="dropdown-item" href="?controlador=usuarios&accion=listar">
                   <i class="bi bi-person-lines-fill"></i>
@@ -66,9 +73,10 @@
                   <span class="ms-2">Gestión de Aranceles</span>
                 </a>
               </li>
-            <?php endif; ?>
-          </ul>
-        </li>
+            </ul>
+          </li>
+        <?php endif; ?>
+
         <li class="nav-item dropdown">
           <a class="nav-link text-uppercase dropdown-toggle <?php echo $controlador == 'usuarios' && $accion == 'logout' ? 'active' : ''; ?>" role="button" data-bs-toggle="dropdown" aria-expanded="true">
             <i class="fas fa-user-circle me-1"></i>
@@ -86,28 +94,28 @@
 
         <!-- Notificaciones dropdown -->
         <?php if (!$esIngeniero): ?>
-        <li class="nav-item dropdown">
-          <a 
-            class="nav-link dropdown-toggle <?php echo $controlador == 'notificaciones' && $accion == 'listar' ? 'active' : ''; ?>"
-             role="button" data-bs-toggle="dropdown" aria-expanded="true" id="notificaciones-header-link">
-            <i class="fas fa-bell"></i>
-            <span class="ms-2 text-uppercase">Notificaciones</span>
-          </a>
-          <ul class="dropdown-menu pb-0" data-bs-popper="static" id="notificaciones-header">
-            <li>
-              <a class="dropdown-item border-bottom" href="">
-                <i class="bi bi-x-circle-fill"></i>
-                No hay notificaciones
-              </a>
-            </li>
-            <li>
-              <a class="dropdown-item border-bottom bg-success-2 text-white" style="border-radius: 0 0 6px 6px;" href="?controlador=notificaciones&accion=listar">
-                <i class="bi bi-bell-fill"></i>
-                <span class="ms-2 ">Mostrar Todas</span>
-              </a>
-            </li>
-          </ul>
-        </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle <?php echo $controlador == 'notificaciones' && $accion == 'listar' ? 'active' : ''; ?>"
+              role="button" data-bs-toggle="dropdown" aria-expanded="true" id="notificaciones-header-link">
+              <i class="fas fa-bell"></i>
+              <span class="ms-2 text-uppercase">Notificaciones</span>
+            </a>
+            <ul class="dropdown-menu pb-0" data-bs-popper="static" id="notificaciones-header">
+              <li>
+                <a class="dropdown-item border-bottom" href="">
+                  <i class="bi bi-x-circle-fill"></i>
+                  No hay notificaciones
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item border-bottom bg-success-2 text-white" style="border-radius: 0 0 6px 6px;" href="?controlador=notificaciones&accion=listar">
+                  <i class="bi bi-bell-fill"></i>
+                  <span class="ms-2 ">Mostrar Todas</span>
+                </a>
+              </li>
+            </ul>
+          </li>
         <?php endif; ?>
         <!-- Fin Notificaciones dropdown -->
 
