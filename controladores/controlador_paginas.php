5dcb4b0dc12481fdc4e3f262dcee60b2ebdbd7cf
davidpereyra2016 <davidpereyra2016.dp@gmail.com>
2025-05-06 19:40:59 -0300

    quite funciones que nada que ver

--- controladores/controlador_paginas.php
+++ controladores/controlador_paginas.php
@@ -4,36 +4,13 @@ include_once("conexion.php");
 
 class ControladorPaginas
 {
-    private $modelo;
-    private $conexion;
-
     public function __construct() {
-        $this->modelo = new ModeloDashboard();
-        $this->conexion = BD::crearInstancia();
     }
     public function inicio() {
         
-
         // Incluir la vista y pasar los datos
         include_once("vistas/template.php");
         include_once("vistas/paginas/inicio.php");
-    }
-
-   
-
-    private function prepararDatosGraficoCategorias($ventas_categoria) {
-        $labels = array();
-        $datos = array();
-
-        foreach ($ventas_categoria as $categoria) {
-            $labels[] = $categoria['categoria'];
-            $datos[] = floatval($categoria['total_ingresos']);
-        }
-
-        return array(
-            'labels' => $labels,
-            'datos' => $datos
-        );
     }
 
     public function error()
